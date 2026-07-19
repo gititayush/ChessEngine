9076c8c93c199c1519a1d9ae02e02d3a35605f74
gititayush <kayush2512@gmail.com>
2026-07-20 02:15:20 +0530

    Initial project setup

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "========================================\n";
+    std::cout << "      AYU CHESS ENGINE v0.0.1\n";
+    std::cout << "========================================\n";
+
+    return 0;
+}
